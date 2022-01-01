d342831a0dfb0b34ee790973f7d4c3f0d49f928b
neyoung <ngozi.eze.young@gmail.com>
2022-01-01 11:33:08 -0500

    init commit

---
+++ gift-manager/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './boilerplate/header/header.component';
+import { NavbarComponent } from './boilerplate/navbar/navbar.component';
+import { FooterComponent } from './boilerplate/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
